8c31b6355611958d6b76de12c512e66c5da74ae7
Rony Santiago Bañol <rony.banolueda.edu.co>
2022-04-05 16:48:38 -0500

    A Quiroz no le conecta

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -10,40 +10,6 @@ export async function getServerSideProps(context) {
   };
 }
 
-const index = () => {
-  const { data, loading } = useQuery(GET_PROJECTS, {
-    fetchPolicy: "cache-and-network",
-  });
-
-  if (loading) return <div>loading...</div>;
-
-  return (
-    <div className="flex flex-col items-center p-10">
-      <Link href="/clients/new" passHref>
-        <div className="self-end button-primary">Nuevo Cliente</div>
-      </Link>
-
-      <h2 className="my-4 text-3xl font-bold text-gray-800">Clientes</h2>
-      <div className="hidden lg:block">
-        <table>
-          <thead>
-            <th>ID Cliente</th>
-
-            <th>Nombre</th>
-            <th>Fecha de Actualización</th>
-            <th>Fecha de Creación</th>
-
-            <th>Acciones</th>
-          </thead>
-          <tbody>
-            {/* {data.getProjects.map((c) => (
-              <div key={c.id}>{c.id} = {c.name}</div>
-            ))} */}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
-};
+const index = () => {};
 
 export default index;
