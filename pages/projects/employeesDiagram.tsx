af5c79d498cde5c4c39e1dcb2712d214b6993a07
quiroz911 <jpablo.quiroz@udea.edu.co>
2022-04-13 11:52:40 -0500

    useEffect any

--- pages/projects/employeesDiagram.tsx
+++ pages/projects/employeesDiagram.tsx
@@ -1,86 +1,96 @@
-import React, { useEffect, useState } from 'react'
-import { matchRoles } from 'utils/matchRoles';
-import dynamic from 'next/dynamic';
-import { useQuery } from '@apollo/client';
-import { GET_DIAGRAM_DATA } from 'graphql/queries/employeesDiagram';
+import React, { useEffect, useState } from "react";
+import { matchRoles } from "utils/matchRoles";
+import dynamic from "next/dynamic";
+import { useQuery } from "@apollo/client";
+import { GET_DIAGRAM_DATA } from "graphql/queries/employeesDiagram";
 //import ReactApexChart from 'react-apexcharts';
 
-const ReactApexChart = dynamic(() => {
-  return import('react-apexcharts');
-
-},{ssr: false});
+const ReactApexChart = dynamic(
+  () => {
+    return import("react-apexcharts");
+  },
+  { ssr: false }
+);
 
 const EmployeesDiagram = () => {
-  const {data, loading} = useQuery(GET_DIAGRAM_DATA);
-  const [options,setOptions] = useState({
-    annotations: {
-    },
+  const { data, loading } = useQuery(GET_DIAGRAM_DATA);
+  const [options, setOptions] = useState<any>({
+    annotations: {},
     chart: {
       height: 350,
-      type: 'bar',
+      type: "bar",
     },
     plotOptions: {
       bar: {
         borderRadius: 10,
-        columnWidth: '50%',
-      }
+        columnWidth: "50%",
+      },
     },
     dataLabels: {
-      enabled: false
+      enabled: false,
     },
     stroke: {
-      width: 2
+      width: 2,
     },
-    
+
     grid: {
       row: {
-        colors: ['#fff', '#f2f2f2']
-      }
+        colors: ["#fff", "#f2f2f2"],
+      },
     },
     xaxis: {
       labels: {
-        rotate: -45
+        rotate: -45,
       },
       categories: [],
-      tickPlacement: 'on'
+      tickPlacement: "on",
     },
     yaxis: {
       title: {
-        text: 'Cantidad de empleados',
+        text: "Cantidad de empleados",
       },
     },
     fill: {
-      type: 'gradient',
+      type: "gradient",
       gradient: {
-        shade: 'light',
+        shade: "light",
         type: "horizontal",
         shadeIntensity: 0.25,
         gradientToColors: undefined,
         inverseColors: true,
         opacityFrom: 0.85,
         opacityTo: 0.85,
-        stops: [50, 0, 100]
+        stops: [50, 0, 100],
       },
-    }
+    },
   });
-  const [series,setSeries] = useState([]);
+  const [series, setSeries] = useState([]);
 
   useEffect(() => {
-    if(data){
+    if (data) {
       setSeries(data.getDiagramData.series);
-      setOptions({...options, xaxis: {...options.xaxis, categories: data.getDiagramData.categories}});
+      setOptions({
+        ...options,
+        xaxis: { ...options.xaxis, categories: data.getDiagramData.categories },
+      });
     }
   }, [data]);
 
-  if(loading) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>;
 
   return (
-    <div className='w-full flex flex-col items-center'>
-        <h1>Diagrama del número de empleados por proyecto</h1>
-        <div className='w-full'>
-        <ReactApexChart options={options} series={series} type="bar" height={350} />    </div>
-        </div>
-  )
-}
+    <div className="w-full flex flex-col items-center">
+      <h1>Diagrama del número de empleados por proyecto</h1>
+      <div className="w-full">
+        <ReactApexChart
+          options={options}
+          series={series}
+          type="bar"
+          height={350}
+        />{" "}
+      </div>
+    </div>
+  );
+};
 
 export default EmployeesDiagram;
